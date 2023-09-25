851e6088229c1ffdd33ac307d4c7fb648a1be036
lsy0194 <tktmadltmddu@naver.com>
2023-09-25 16:15:30 +0900

    좌우버튼 클릭시 박스회전

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import './style.scss';
 import { useState } from 'react';
-function App() {
-	console.log('app');
-	//const [상태값, 상태변경전용함수] = useState(초기값);
-	//리액트 컴포넌트는 State값이 State변경함수로 변경되야지만 컴포넌트가 재랜더링됨
-	//숫자를 증가, 감소 시킬때 전위증감 연산자를 써야지만 해당 렌더링 사이클에서 바로 값이 변경되면서 다음번 렌더링에 반영됨
-	//State에 담기는 값이 바뀔때에만 화면의 갱신이 일어나기 때문에
-	//State에 담기는 데이터만 관리하면 되므로 유지보수가 편함
-	let [Num, setNum] = useState(0);
-	console.log(Num);
+import './style.scss';
 
+function App() {
+	const [Degree, setDegree] = useState(0);
 	return (
 		<>
-			<h1>{Num}</h1>
-			<button onClick={() => setNum(--Num)}>minus</button>
-			<button onClick={() => setNum(++Num)}>plus</button>
+			<button onClick={() => setDegree(Degree - 45)}>왼쪽으로 회전</button>
+			<button onClick={() => setDegree(Degree + 45)}>오른쪽으로 회전</button>
+			<article style={{ transform: `rotate(${Degree}deg)` }}>{Degree}</article>
 		</>
 	);
 }
