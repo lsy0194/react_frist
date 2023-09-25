a460f700824f2f9bfb61191d6fcab949cb6c5505
lsy0194 <tktmadltmddu@naver.com>
2023-09-25 15:05:27 +0900

    전개 연산자를 통한 객체 완전복사

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ import './style.scss';
 //리엑트에서 배열이나 객체같은 참조형 자료는 무조건 deep copy를 해서 데이터를 변경해야함
 
 function App() {
-	let arr = ['reading', 'game', 'cook'];
-	//전개연산자 (Spread Operator): heap메모리에 있는 값을 물리적으로 꺼내서 전개
-	//전개연산자를 이용하면 원본을 훼손시키지 않으면 참조형 자료를 deep copy가능
-	let newArr = [...arr];
-	newArr[0] = 'exercise';
-	console.log(newArr);
-	console.log(arr);
-
+	let student = {
+		name: 'David',
+		age: 20,
+	};
+	let newStudent = { ...student };
+	newStudent.name = 'Andy';
+	console.log(newStudent);
+	console.log(student);
 	let isPop = true;
 	let isFooter = true;
 	return (
