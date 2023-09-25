50cd0337d5559dfc785637df5b868b98057e535c
lsy0194 <tktmadltmddu@naver.com>
2023-09-25 15:16:29 +0900

    react에서 자주쓰는 hook정리

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,6 @@ import './style.scss';
 //리엑트에서 배열이나 객체같은 참조형 자료는 무조건 deep copy를 해서 데이터를 변경해야함
 
 function App() {
-	let student = {
-		name: 'David',
-		age: 20,
-	};
-	let newStudent = { ...student };
-	newStudent.name = 'Andy';
-	console.log(newStudent);
-	console.log(student);
 	let isPop = true;
 	let isFooter = true;
 	return (
@@ -42,32 +34,21 @@ function App() {
 export default App;
 
 /*
-	SSR vs CSR
+	hooks
+	-리액트 16버전부터 새로나온 개념으로 리액트에서 자주쓰이는 상태관리, 생명주기에 관련된 내용들을 
+	-hook이라는 형태의 내장함수로 편의기능을 제공
+	-hook이 나오기 전까지는 class방식으로 컴포넌트를 생성 및 기능확장을 비효율적으로 처리
 
-	SSR - Server Side Rendering
-	- 페이지 이동시마다 일일이 서버쪽에 HTML파일을 요청해서 가져오는 방식
-	- 장점 : 초기로딩속도가 빠름, 검색엔진 최적화 (SEO 좋음)
-	- 단점 : 페이지 이동시마다 일일이 서버쪽에 파일을 요청해야 되므로 페이지 깜박거림
+	자주쓰는 hook 3대장
+	useState - 컴포넌트에서 화면의 렌더링을 담당하는 중요한 정보값을 관리
+	useEffect - 컴포넌트의 생명주기에 관련된 함수 (생성,변화,소멸)
+	useRef - 컴포넌트 안쪽에서 특정 값을 참조객체에 담을때
 
-	CSR - Client Side Rendering
-	- 초기에 빈 HTML파일을 서버쪽에서 가져오고 화면에 출력될 모든 데이터를 자바스립트로 Chunk단위의 모든 데이터 파일 가져온후 빈 HTML파일에 동적으로 출력
-	- 장점 : 한번에 서브페이지포함한 모든 데이터를 불러오기 때문에 페이지 이동시마다 새롭게 파일을 요청할 필요가 없기 때문에 페이지 전환이 자연스러움
-	- 단점 : 모든 페이지에 대한 데이터를 한번에 다 가져오기 때문에 초기로딩속도가 SSR방식에 비해서는 늦음, 검색엔진 최적화가 안됨
-
-	Real DOM (실제돔)
-	- HTML태그를 이용해서 구조를 만들면 브라우저가 이를 해석해서 실제 DOM형태로 객체를 만들고 화면에 렌더링
-
-	Virtual DOM (가상돔)
-	- 브라우저에 의해 Real DOM으로 변경되기 전 자바스크립트에 의해서 메모리상으로 가상의 DOM을 만들어서 기존의 DOM구조와 차이점을 분석하고 바뀐 부분을 다시 렌더링하는 형태
-
-	JSX
-	- 리액트에 DOM을 효율적으로 생성하기 위한 HTML의 규칙성을 따라한 자바스크립트 돔 제작 방식
-
-	컴포넌트 생성시 주의점
-	- 무조건 JSX를 리턴
-	- 함수 이름은 대문자로 시작
-	- export 로 내보야지 다른 컴포넌트에서 불러올수 있음
-	- 하나의 컴포넌트 함수는 단인 JSX를 리턴 가능
-	- 복수개의 JSX를 리턴하고 싶을때는 wrapping El로 묶어서 그룹화한뒤 리턴
-	- 중첩된 Element노드를 생성하지 않고 복수개의 JSX를 리턴하고 싶을때는 <></> Fragment로 감싸줌
+	리액트 성능관리를 hook
+	리액트에서의 memoization - 메모리점유율을 늘려서 성능을 개선
+	자바스크립트는 기본적으로 Garbage collector에 의해서 메모리가 관리됨
+	아래의 hook을 통해서 특정값을 강제 메모이제이션 처리하면 가비지컬렉터에서 제외함
+	memo (컴포넌트 자체를 메모이제이션)
+	useCallback (컴포넌트 안쪽의 핸들러 함수 자체를 메모이제이션)
+	useMemo (특정 핸들러함수의 리턴값을 메모이제이션)
 */
