9f994f3646392f9d76247a50cda7e1e0e4175d52
lsy0194 <tktmadltmddu@naver.com>
2023-09-26 09:48:41 +0900

    Pop안에 숫자 증가 이벤트 연결

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,10 +1,27 @@
 import { useEffect } from 'react';
-
+import { useState } from 'react';
 function Popup() {
+	const [Num, setNum] = useState(0);
 	useEffect(() => {
+		//컴포넌트 mount시 한번만 실행
 		console.log('팝업생성');
+		return () => {
+			//컴포넌트가 unmount시 한번만 실행
+			console.log('팝업소멸');
+		};
 	}, []);
-	return <aside>Popup</aside>;
+
+	useEffect(() => {
+		console.log('popup의 Num 상태변화');
+	}, [Num]);
+
+	return (
+		<aside>
+			<h1>{Num}</h1>
+			<button onClick={() => setNum(Num - 1)}>minus</button>
+			<button onClick={() => setNum(Num + 1)}>plus</button>
+		</aside>
+	);
 }
 
 export default Popup;
